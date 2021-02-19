30a9a83a592346f3de1b73aff43184e4989a71eb
Alex Sandoval <sandovalex2001.as@gmail.com>
2021-02-18 20:59:46 -0600

    Se agrego atributos y metodo en la clase Empleados

---
+++ FerreteriaMaresa/Dominio/DMO_Empleados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class DMO_Empleados
+    {
+        private int id_empleado;
+        private string nombre_empleado;
+        private string nombre;
+        private string correo;
+        private int telefono;
+        private string direccion;
+        private int id_usuario;
+
+        public DMO_Empleados()
+        {
+
+        }
+    }
+}
