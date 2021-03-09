140b488bdbdc878417ccdb0b7dbefa5116fe59a2
Danny Martinez <Danny@DANNY>
2021-03-09 01:53:11 -0600

    Noe Ignacio, Jose Marin

--- FerreteriaMaresa/Presentacion/RegistroClientes.cs
+++ FerreteriaMaresa/Presentacion/RegistroClientes.cs
@@ -11,56 +11,63 @@ namespace Presentacion
 
         private void habilitar_Click_1(object sender, System.EventArgs e)
         {
-            btnhabilitar.Enabled = false;
+            btnGuardar.Visible = false;
             btnEliminar.Enabled = true;
             btnModificar.Enabled = true;
-            btnMostrar.Enabled = true;
             btnAgregar.Enabled = true;
 
             nombre.Enabled = false;
             Apellido.Enabled = false;
             rtn.Enabled = false;
-            direccion.Enabled = false;
-            telefono.Enabled = false;
+            txtDireccion.Enabled = false;
+            txtTelefono.Enabled = false;
         }
 
         private void button6_Click(object sender, System.EventArgs e)
         {
-
+            btnAgregar.Enabled = false;
+            btnModificar.Enabled = false;
+            btnGuardar.Visible = true;
         }
 
         private void btnAgregar_Click(object sender, System.EventArgs e)
         {
             btnEliminar.Enabled = false;
             btnModificar.Enabled = false;
-            btnMostrar.Enabled = false;
-            btnhabilitar.Enabled = true;
+            btnGuardar.Visible = true;
+            btnGuardar.Enabled = true;
 
             nombre.Enabled = true;
             Apellido.Enabled = true;
             rtn.Enabled = true;
-            direccion.Enabled = true;
-            telefono.Enabled = true;
-        }
-
-        private void btnMostrar_Click(object sender, System.EventArgs e)
-        {
-            btnAgregar.Enabled = false;
-            btnEliminar.Enabled = false;
-            btnhabilitar.Enabled = true;
+            txtDireccion.Enabled = true;
+            txtTelefono.Enabled = true;
         }
 
         private void btnModificar_Click(object sender, System.EventArgs e)
         {
             btnAgregar.Enabled = false;
             btnEliminar.Enabled = false;
-            btnhabilitar.Enabled = true;
+            btnGuardar.Visible = true;
 
             nombre.Enabled = true;
             Apellido.Enabled = true;
             rtn.Enabled = true;
-            direccion.Enabled = true;
-            telefono.Enabled = true;
+            txtDireccion.Enabled = true;
+            txtTelefono.Enabled = true;
+        }
+
+        private void dgvEmpleados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nombre.Text = dgvEmpleados.CurrentRow.Cells[1].Value.ToString();
+            Apellido.Text = dgvEmpleados.CurrentRow.Cells[2].Value.ToString();
+            rtn.Text = dgvEmpleados.CurrentRow.Cells[3].Value.ToString();
+            txtDireccion.Text = dgvEmpleados.CurrentRow.Cells[4].Value.ToString();
+            txtCiudad.Text = dgvEmpleados.CurrentRow.Cells[5].Value.ToString();
+            txtRegion.Text = dgvEmpleados.CurrentRow.Cells[6].Value.ToString();
+            txtCodPost.Text = dgvEmpleados.CurrentRow.Cells[7].Value.ToString();
+            txtPais.Text = dgvEmpleados.CurrentRow.Cells[8].Value.ToString();
+            txtTelefono.Text = dgvEmpleados.CurrentRow.Cells[9].Value.ToString();
         }
     }
 }
--- FerreteriaMaresa/Presentacion/VentaProducto.cs
+++ FerreteriaMaresa/Presentacion/VentaProducto.cs
@@ -23,5 +23,14 @@ namespace Presentacion
             precio.Enabled = true;
             Cantidad.Enabled = true;
         }
+
+        private void dgvEmpleados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Id.Text = dgvProducto.CurrentRow.Cells[1].Value.ToString();
+            Nombre.Text = dgvProducto.CurrentRow.Cells[2].Value.ToString();
+            Strock.Text = dgvProducto.CurrentRow.Cells[3].Value.ToString();
+            precio.Text = dgvProducto.CurrentRow.Cells[4].Value.ToString();
+            Cantidad.Text = dgvProducto.CurrentRow.Cells[5].Value.ToString();
+        }
     }
 }
