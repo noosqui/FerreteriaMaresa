351587a17fc26fa256bf9ad34226167d01202ce5
Alex Sandoval <sandovalex2001.as@gmail.com>
2021-03-10 23:00:35 -0600

    Avances Buscar Inventario

--- FerreteriaMaresa/Presentacion/Inventario.cs
+++ FerreteriaMaresa/Presentacion/Inventario.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
 
 namespace Presentacion
 {
@@ -7,6 +10,46 @@ namespace Presentacion
         public Inventario()
         {
             InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, System.EventArgs e)
+        {
+            string searchValue = txtcodigo.Text;
+
+            dgvInventario.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            try
+            {
+                dgvInventario.ClearSelection();
+                foreach (DataGridViewRow row in dgvInventario.Rows)
+                {
+                    try
+                    {
+                        if (row.Cells[0].Value.ToString().Equals(searchValue))
+                        {
+                            row.Selected = true;
+                            break;
+                        }
+                        else
+                        {
+                            string searchValue1 = txtnombre.Text;
+                            if (row.Cells[1].Value.ToString().Equals(searchValue1))
+                            {
+                                row.Selected = true;
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show("No se encontro el Codigo "+exc);
+                    }
+
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("No se encontro el Codigo " + exc);
+            }
         }
     }
 }
