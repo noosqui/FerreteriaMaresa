0b9b7686dff917f4f28f400dc575d15802494e0c
noosqui <79149159+noosqui@users.noreply.github.com>
2021-03-24 07:25:46 -0600

    Revert "Avance por Lesvin Ardon"

--- FerreteriaMaresa/Dominio/DOM_Clientes.cs
+++ FerreteriaMaresa/Dominio/DOM_Clientes.cs
@@ -12,7 +12,7 @@ namespace Dominio
     public class DOM_Clientes
 
     {
-        private string idCliente;
+        private int idCliente;
         private string nombrecliente;
         private string apellidocliente;
         private string RTN;
@@ -24,28 +24,18 @@ namespace Dominio
         private string pais;
         private CD_Clientes cli = new CD_Clientes();
 
-        public DOM_Clientes() { 
-        
-        }
-
-        public DOM_Clientes(string id_Cliente, string nombre, string apellido, string rtn, string telefono, string direccion,
-            string ciudad, string region, string codigo_postal, string pais)
+        public DOM_Clientes(string Cliente, string nombre, string apellido)
         {
-            this.idCliente = id_Cliente;
+            this.idCliente = int.Parse(Cliente);
             this.nombrecliente = nombre;
             this.apellidocliente = apellido;
-            this.RTN = rtn;
-            this.telefono = telefono;
-            this.direccion = direccion;
-            this.ciudad = ciudad;
-            this.region = region;
-            this.codigo_postal = codigo_postal;
-            this.pais = pais;
-
+        }
+        public DOM_Clientes ()
+        {
 
         }
 
-        public string Id_cliente
+        public int Id_empleado
         {
             get { return idCliente; }
             set { idCliente = value; }
@@ -60,7 +50,7 @@ namespace Dominio
             get { return apellidocliente; }
             set { apellidocliente = value; }
         }
-        public string Rtn
+        public string Correo
         {
             get { return RTN; }
             set { RTN = value; }
@@ -74,41 +64,18 @@ namespace Dominio
         {
             get { return direccion; }
             set { direccion = value; }
-        }     
-        public string Ciudad 
-        {
-            get { return ciudad;  }
-            set { ciudad = value; }
-        }
-        public string Region 
-        {
-            get { return region; }
-            set { region = value; }
-        }
-        public string Codigo_Postal 
-        {
-            get { return codigo_postal; }
-            set { codigo_postal = value; }
-        }
-        public string Pais
-        {
-            get { return pais; }
-            set { pais = value; }
         }
 
 
-        public void editar_Clientes(string idCliente, string nombrecliente, string apellidocliente, string RTN, string telefono,
-            string direccion, string ciudad, string region, string codigo_postal, string pais)
+        public void editar_Clientes()
         {
             cli.Editar_Cliente(idCliente, nombrecliente, apellidocliente, RTN, telefono, direccion, ciudad, region, codigo_postal, long.Parse(pais));
         }
-        public void eliminar_empleado(string idCliente)
+        public void eliminar_empleado()
         {
             cli.Eliminar_Cliente(idCliente);
         }
-
-        public void crear_Cliente(string idCliente, string nombrecliente, string apellidocliente,string RTN, string telefono, 
-            string direccion, string ciudad, string region, string codigo_postal, string pais)
+        public void crear_Cliente()
         {
             cli.insertar_Cliente(idCliente, nombrecliente, apellidocliente, RTN, telefono, direccion, ciudad, region, codigo_postal, long.Parse(pais));
         }
@@ -117,9 +84,9 @@ namespace Dominio
         {
             return cli.Mostrar_Cliente();
         }
-        public DataTable Buscar_Cliente()
+        public void Buscar_Cliente()
         {
-            return cli.buscar_Clientes(idCliente);
+            cli.buscar_Clientes(idCliente);
         }
     }
 }
