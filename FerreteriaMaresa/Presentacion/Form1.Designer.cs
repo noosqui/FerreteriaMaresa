f32acdda43811a396fab766c120a5adff21b7ba4
Alex Sandoval <sandovalex2001.as@gmail.com>
2021-03-24 02:30:52 -0600

    Arreglos form empleados, tab, busqueda,orden text

--- FerreteriaMaresa/Presentacion/Form1.Designer.cs
+++ FerreteriaMaresa/Presentacion/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace Presentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(813, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
