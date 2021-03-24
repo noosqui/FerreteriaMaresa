76dff18c31f50e89dfae65e9ca0088c01866c7bb
Alex Sandoval <sandovalex2001.as@gmail.com>
2021-03-24 10:11:07 -0600

    Actualizacion empleados

--- FerreteriaMaresa/Presentacion/RegistroClientes.Designer.cs
+++ FerreteriaMaresa/Presentacion/RegistroClientes.Designer.cs
@@ -59,12 +59,15 @@ namespace Presentacion
             this.nombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnGuardar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(23)))), ((int)(((byte)(74)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnModificar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
@@ -216,7 +219,7 @@ namespace Presentacion
             this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnModificar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnModificar.Location = new System.Drawing.Point(413, 465);
+            this.btnModificar.Location = new System.Drawing.Point(804, 309);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(89, 33);
             this.btnModificar.TabIndex = 58;
@@ -388,7 +391,7 @@ namespace Presentacion
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(620, 53);
+            this.label1.Location = new System.Drawing.Point(595, 45);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(141, 39);
@@ -432,7 +435,6 @@ namespace Presentacion
             this.Controls.Add(this.txtnombrecli);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txttelefono);
@@ -446,13 +448,14 @@ namespace Presentacion
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.dgvClientes);
             this.Controls.Add(this.nombre);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RegistroClientes";
             this.Text = "RegistroClientes";
             this.Load += new System.EventHandler(this.RegistroClientes_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
