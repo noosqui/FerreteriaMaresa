b89d215f2b478366a1867e8b4fb5687acb76af62
Jafet-Reyes <aj_reyesm@gmail.com>
2021-04-02 22:23:04 -0600

    Inventario FMR

--- FerreteriaMaresa/Datos/CD_Inventario.cs
+++ FerreteriaMaresa/Datos/CD_Inventario.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+//using System.Windows.Forms;
+
 namespace Datos
 {
      public class CD_Inventario
 
-    {
+     {
         private CD_Conexion conexion = new CD_Conexion();
         SqlDataReader lee;
         DataTable tabla = new DataTable();
@@ -17,11 +19,13 @@ namespace Datos
 
         public DataTable Mostrar_Inventario()
         {
+            DataTable tabla = new DataTable();
             comando.Connection = conexion.abrir();
             comando.CommandText = "SP_Inventario";
             comando.CommandType = CommandType.StoredProcedure;
             lee = comando.ExecuteReader();
             tabla.Load(lee);
+            lee.Close();
             comando.Connection = conexion.cerrar();
             return tabla;
         }
@@ -60,11 +64,13 @@ namespace Datos
         }
         public DataTable Mostrar_Marcas()
         {
+            DataTable tabla = new DataTable();
             comando.Connection = conexion.abrir();
             comando.CommandText = "Mostrar_Marcas";
             comando.CommandType = CommandType.StoredProcedure;
             lee = comando.ExecuteReader();
             tabla.Load(lee);
+            lee.Close();
             comando.Connection = conexion.cerrar();
             return tabla;
         }
@@ -84,11 +90,13 @@ namespace Datos
 
         public DataTable Mostrar_Categorias()
         {
+            DataTable tabla = new DataTable();
             comando.Connection = conexion.abrir();
             comando.CommandText = "Mostrar_Categorias";
             comando.CommandType = CommandType.StoredProcedure;
             lee = comando.ExecuteReader();
             tabla.Load(lee);
+            lee.Close();
             comando.Connection = conexion.cerrar();
             return tabla;
         }
@@ -105,5 +113,24 @@ namespace Datos
             comando.Connection = conexion.cerrar();
             return tabla;
         }
+
+        /*public void LlenarMarca(ComboBox cb)
+        {
+            try
+            {
+                comando = new SqlCommand("Select descripcion from Marcas", conexion.abrir());
+                lee = comando.ExecuteReader();
+                while (lee.Read())
+                {
+                    cb.Items.Add(dr["descripcion"].ToString());
+                }
+                lee.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se lleno el Combobox" + ex.ToString());
+            }
+        }*/
+
     }
 }
--- FerreteriaMaresa/Presentacion/Inventario.Designer.cs
+++ FerreteriaMaresa/Presentacion/Inventario.Designer.cs
@@ -34,22 +34,24 @@
             this.label8 = new System.Windows.Forms.Label();
             this.txtnombre = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.btnBuscar = new System.Windows.Forms.Button();
             this.btnActivos = new System.Windows.Forms.Button();
             this.btnTodos = new System.Windows.Forms.Button();
             this.btnInactivos = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvInventario
             // 
             this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInventario.Location = new System.Drawing.Point(139, 163);
+            this.dgvInventario.Location = new System.Drawing.Point(148, 195);
             this.dgvInventario.Name = "dgvInventario";
             this.dgvInventario.RowHeadersWidth = 51;
             this.dgvInventario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvInventario.Size = new System.Drawing.Size(746, 354);
+            this.dgvInventario.Size = new System.Drawing.Size(885, 409);
             this.dgvInventario.TabIndex = 23;
             this.dgvInventario.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmpleados_CellClick);
             this.dgvInventario.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInventario_CellContentClick);
@@ -59,15 +61,15 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(416, 9);
+            this.label1.Location = new System.Drawing.Point(421, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(134, 31);
+            this.label1.Size = new System.Drawing.Size(182, 31);
             this.label1.TabIndex = 34;
-            this.label1.Text = "Inventario";
+            this.label1.Text = "INVENTARIO";
             // 
             // txtcodigo
             // 
-            this.txtcodigo.Location = new System.Drawing.Point(230, 127);
+            this.txtcodigo.Location = new System.Drawing.Point(230, 56);
             this.txtcodigo.MaxLength = 9;
             this.txtcodigo.Name = "txtcodigo";
             this.txtcodigo.Size = new System.Drawing.Size(58, 20);
@@ -80,17 +82,17 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label8.Location = new System.Drawing.Point(136, 128);
+            this.label8.Location = new System.Drawing.Point(136, 56);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(88, 16);
+            this.label8.Size = new System.Drawing.Size(81, 16);
             this.label8.TabIndex = 32;
-            this.label8.Text = "Id Empleado:";
+            this.label8.Text = "ID Producto:";
             // 
             // txtnombre
             // 
-            this.txtnombre.Location = new System.Drawing.Point(375, 127);
+            this.txtnombre.Location = new System.Drawing.Point(202, 99);
             this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(398, 20);
+            this.txtnombre.Size = new System.Drawing.Size(171, 20);
             this.txtnombre.TabIndex = 31;
             this.txtnombre.TextChanged += new System.EventHandler(this.txtnombre_TextChanged_1);
             this.txtnombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtnombre_KeyPress);
@@ -100,28 +102,11 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(309, 131);
+            this.label7.Location = new System.Drawing.Point(136, 99);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 16);
             this.label7.TabIndex = 30;
             this.label7.Text = "Nombre:";
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
-            this.btnBuscar.FlatAppearance.BorderSize = 0;
-            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnBuscar.Location = new System.Drawing.Point(801, 119);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(0);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(84, 28);
-            this.btnBuscar.TabIndex = 29;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnBuscar.UseVisualStyleBackColor = false;
             // 
             // btnActivos
             // 
@@ -130,12 +115,12 @@
             this.btnActivos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnActivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnActivos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnActivos.Location = new System.Drawing.Point(20, 200);
+            this.btnActivos.Location = new System.Drawing.Point(11, 272);
             this.btnActivos.Margin = new System.Windows.Forms.Padding(2);
             this.btnActivos.Name = "btnActivos";
-            this.btnActivos.Size = new System.Drawing.Size(67, 27);
+            this.btnActivos.Size = new System.Drawing.Size(123, 57);
             this.btnActivos.TabIndex = 58;
-            this.btnActivos.Text = "Activos";
+            this.btnActivos.Text = "ACTIVOS";
             this.btnActivos.UseVisualStyleBackColor = false;
             this.btnActivos.Click += new System.EventHandler(this.btnActivos_Click);
             this.btnActivos.StyleChanged += new System.EventHandler(this.btnActivos_StyleChanged);
@@ -147,12 +132,12 @@
             this.btnTodos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnTodos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTodos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnTodos.Location = new System.Drawing.Point(20, 332);
+            this.btnTodos.Location = new System.Drawing.Point(11, 355);
             this.btnTodos.Margin = new System.Windows.Forms.Padding(2);
             this.btnTodos.Name = "btnTodos";
-            this.btnTodos.Size = new System.Drawing.Size(67, 27);
+            this.btnTodos.Size = new System.Drawing.Size(123, 56);
             this.btnTodos.TabIndex = 59;
-            this.btnTodos.Text = "Todos";
+            this.btnTodos.Text = "TODOS";
             this.btnTodos.UseVisualStyleBackColor = false;
             this.btnTodos.Click += new System.EventHandler(this.btnTodos_Click);
             // 
@@ -163,23 +148,76 @@
             this.btnInactivos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInactivos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInactivos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnInactivos.Location = new System.Drawing.Point(20, 261);
+            this.btnInactivos.Location = new System.Drawing.Point(11, 195);
             this.btnInactivos.Margin = new System.Windows.Forms.Padding(2);
             this.btnInactivos.Name = "btnInactivos";
-            this.btnInactivos.Size = new System.Drawing.Size(67, 27);
+            this.btnInactivos.Size = new System.Drawing.Size(123, 53);
             this.btnInactivos.TabIndex = 60;
-            this.btnInactivos.Text = "Inactivos";
+            this.btnInactivos.Text = "INACTIVOS";
             this.btnInactivos.UseVisualStyleBackColor = false;
             this.btnInactivos.Click += new System.EventHandler(this.btnInactivos_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(23)))), ((int)(((byte)(74)))));
+            this.panel1.Controls.Add(this.cmbCategoria);
+            this.panel1.Controls.Add(this.cmbMarca);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dgvInventario);
+            this.panel1.Controls.Add(this.btnTodos);
+            this.panel1.Controls.Add(this.txtnombre);
+            this.panel1.Controls.Add(this.txtcodigo);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.btnInactivos);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.btnActivos);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1036, 602);
             this.panel1.TabIndex = 0;
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "ABRAZADERA",
+            "ACEITES Y LUBRICANTES",
+            "HIDRO LAVADORAS",
+            "TOLDOS",
+            "BICICLETAS",
+            "ACCESORIOS DE AUTOMOTRIZ",
+            "ACCESORIOS PARA PUERTA",
+            "ADHESIVOS Y PEGAMENTOS",
+            "TENSORES",
+            "PINTURAS, ANTICORROSIVOS",
+            "LAMINA DESPLEGADA",
+            "CEMENTO",
+            "CANALETA",
+            "BAÑERAS",
+            "SECADOR DE MANO",
+            "EQUIPO DE FONTANERÍA"});
+            this.cmbCategoria.Location = new System.Drawing.Point(622, 99);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(199, 21);
+            this.cmbCategoria.TabIndex = 69;
+            this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Items.AddRange(new object[] {
+            "SANTUL",
+            "TRUPER",
+            "CHEVRON",
+            "AKSI",
+            "ELECLITE",
+            "FLYING EAGLE"});
+            this.cmbMarca.Location = new System.Drawing.Point(391, 99);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(212, 21);
+            this.cmbMarca.TabIndex = 68;
+            this.cmbMarca.SelectedIndexChanged += new System.EventHandler(this.cmbMarca_SelectedIndexChanged);
             // 
             // Inventario
             // 
@@ -187,24 +225,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
             this.ClientSize = new System.Drawing.Size(1036, 602);
-            this.Controls.Add(this.btnInactivos);
-            this.Controls.Add(this.btnTodos);
-            this.Controls.Add(this.btnActivos);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtcodigo);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.txtnombre);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.dgvInventario);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Inventario";
             this.Text = "Inventario";
             this.Load += new System.EventHandler(this.Inventario_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -216,10 +245,11 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox txtnombre;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.Button btnActivos;
         private System.Windows.Forms.Button btnTodos;
         private System.Windows.Forms.Button btnInactivos;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbMarca;
     }
 }
